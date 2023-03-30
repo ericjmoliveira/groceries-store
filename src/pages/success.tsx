28e95c478cb849b6a2ec32f73938f473927d720b
Eric Oliveira <ericjmoliveira@gmail.com>
2023-03-30 20:40:26 -0300

    Create success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { useCartStore } from '@/store/cart';
+import { useEffect } from 'react';
 
 export default function Success() {
+  const clearCart = useCartStore((state) => state.clearCart);
+
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
   return (
     <>
       <Head>
         <title>Success</title>
       </Head>
-      <span>Success</span>
+      <h2 className="text-2xl font-semibold">Success</h2>
+      <div className="flex flex-col items-center justify-center mt-32 md:mt-16">
+        <span className="text-lg font-semibold mb-4">Purchase successfull</span>
+        <span className="mb-4">Thank you for your purchase!</span>
+        <Image src={'./empty-cart.svg'} width={250} height={250} alt="Empty cart" />
+        <Link href={'/'} className="underline">
+          Continue shopping
+        </Link>
+      </div>
     </>
   );
 }
