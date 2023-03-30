429978696503c8ad0444f321cb907cda62fe2da6
Eric Oliveira <ericjmoliveira@gmail.com>
2023-03-30 10:43:21 -0300

    Initial header styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export function Header() {
   return (
-    <header>
-      <span>Header</span>
+    <header className="w-full p-4 md:p-8 bg-blue-600 text-white shadow-md">
+      <Link href={'/'} className="flex items-center gap-2">
+        <span className="text-2xl font-medium">Wowmart</span>
+        <Image src={'/spark.png'} width={33} height={33} alt="Wowmart spark" />
+      </Link>
     </header>
   );
 }
