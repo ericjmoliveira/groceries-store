320b618d1b03db2ff5f4ccf4544f61919fd2dd8e
Eric Oliveira <ericjmoliveira@gmail.com>
2023-03-31 16:09:25 -0300

    Update sign in/sign up pages

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -11,6 +11,7 @@ export default function SignIn() {
     password: useRef<HTMLInputElement>(null)
   };
 
+  const authenticated = useAuthStore((state) => state.authenticated);
   const handleSignIn = useAuthStore((state) => state.handleSignIn);
 
   const handleSubmit = async (event: FormEvent) => {
@@ -25,45 +26,47 @@ export default function SignIn() {
   return (
     <>
       <Head>
-        <title>Sign up</title>
+        <title>Sign in</title>
       </Head>
-      <div className="w-10/12 md:w-1/3 mt-8 flex flex-col items-center justify-center mx-auto">
-        <Link href={'/'} className="flex flex-col items-center justify-center mb-2">
-          <Image src={'/spark.png'} width={30} height={30} alt="Wowmart spark" />
-        </Link>
-        <h2 className="text-lg font-semibold mb-8">Sign in to your Wowmart account</h2>
-        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 mb-4">
-          <section>
-            <input
-              type="email"
-              placeholder="Your email"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.email}
-            />
-          </section>
-          <section>
-            <input
-              type="password"
-              placeholder="Your password"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.password}
-            />
-          </section>
-          <button className="flex items-center justify-center my-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-full hover:bg-blue-800 transition">
-            Sign in
-          </button>
-        </form>
-        <p className="font-medium">
-          Do not have an account?
-          <Link href={'/signup'} className="ml-1 text-blue-600 font-medium hover:underline">
-            Create an account
+      {!authenticated && (
+        <div className="w-10/12 md:w-1/3 mt-8 flex flex-col items-center justify-center mx-auto">
+          <Link href={'/'} className="flex flex-col items-center justify-center mb-2">
+            <Image src={'/spark.png'} width={30} height={30} alt="Wowmart spark" />
           </Link>
-        </p>
-      </div>
+          <h2 className="text-lg font-semibold mb-8">Sign in to your Wowmart account</h2>
+          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 mb-4">
+            <section>
+              <input
+                type="email"
+                placeholder="Your email"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.email}
+              />
+            </section>
+            <section>
+              <input
+                type="password"
+                placeholder="Your password"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.password}
+              />
+            </section>
+            <button className="flex items-center justify-center my-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-full hover:bg-blue-800 transition">
+              Sign in
+            </button>
+          </form>
+          <p className="font-medium">
+            Do not have an account?
+            <Link href={'/signup'} className="ml-1 text-blue-600 font-medium hover:underline">
+              Create an account
+            </Link>
+          </p>
+        </div>
+      )}
     </>
   );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -14,6 +14,7 @@ export default function SignUp() {
     confirmPassword: useRef<HTMLInputElement>(null)
   };
 
+  const authenticated = useAuthStore((state) => state.authenticated);
   const handleSignUp = useAuthStore((state) => state.handleSignUp);
 
   const handleSubmit = async (event: FormEvent) => {
@@ -37,73 +38,75 @@ export default function SignUp() {
       <Head>
         <title>Sign up</title>
       </Head>
-      <div className="w-10/12 md:w-1/3 mt-8 flex flex-col items-center justify-center mx-auto">
-        <Link href={'/'} className="flex flex-col items-center justify-center mb-2">
-          <Image src={'/spark.png'} width={30} height={30} alt="Wowmart spark" />
-        </Link>
-        <h2 className="text-lg font-semibold mb-8">Create your Wowmart account</h2>
-        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 mb-4">
-          <section>
-            <input
-              type="text"
-              placeholder="Your first name"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.firstName}
-            />
-          </section>
-          <section>
-            <input
-              type="text"
-              placeholder="Your last name"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.lastName}
-            />
-          </section>
-          <section>
-            <input
-              type="email"
-              placeholder="Your email"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.email}
-            />
-          </section>
-          <section>
-            <input
-              type="password"
-              placeholder="Your password (min: 6 characters)"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.password}
-            />
-          </section>
-          <section>
-            <input
-              type="password"
-              placeholder="Confirm the password"
-              autoComplete="off"
-              required
-              className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
-              ref={formRefs.confirmPassword}
-            />
-          </section>
-          <button className="flex items-center justify-center my-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-full hover:bg-blue-800 transition">
-            Sign up
-          </button>
-        </form>
-        <p className="font-medium">
-          Already have an account?
-          <Link href={'/signin'} className="ml-1 text-blue-600 font-medium hover:underline">
-            Sign in
+      {!authenticated && (
+        <div className="w-10/12 md:w-1/3 mt-8 flex flex-col items-center justify-center mx-auto">
+          <Link href={'/'} className="flex flex-col items-center justify-center mb-2">
+            <Image src={'/spark.png'} width={30} height={30} alt="Wowmart spark" />
           </Link>
-        </p>
-      </div>
+          <h2 className="text-lg font-semibold mb-8">Create your Wowmart account</h2>
+          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 mb-4">
+            <section>
+              <input
+                type="text"
+                placeholder="Your first name"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.firstName}
+              />
+            </section>
+            <section>
+              <input
+                type="text"
+                placeholder="Your last name"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.lastName}
+              />
+            </section>
+            <section>
+              <input
+                type="email"
+                placeholder="Your email"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.email}
+              />
+            </section>
+            <section>
+              <input
+                type="password"
+                placeholder="Your password (min: 6 characters)"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.password}
+              />
+            </section>
+            <section>
+              <input
+                type="password"
+                placeholder="Confirm the password"
+                autoComplete="off"
+                required
+                className="w-full px-4 py-2 bg-transparent border border-gray-500 rounded-lg"
+                ref={formRefs.confirmPassword}
+              />
+            </section>
+            <button className="flex items-center justify-center my-4 px-4 py-2 bg-blue-600 text-white font-medium rounded-full hover:bg-blue-800 transition">
+              Sign up
+            </button>
+          </form>
+          <p className="font-medium">
+            Already have an account?
+            <Link href={'/signin'} className="ml-1 text-blue-600 font-medium hover:underline">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      )}
     </>
   );
 }
