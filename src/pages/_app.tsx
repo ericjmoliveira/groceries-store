c9c3f028972116c00d577fde77dbf7d645dcdb2f
Eric Oliveira <ericjmoliveira@gmail.com>
2023-04-01 14:28:51 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { useCartStore } from '@/store/cart';
 import { useProductsStore } from '@/store/products';
@@ -8,25 +8,15 @@ import { Layout } from '@/components/layout';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(true);
-
   const populateCart = useCartStore((state) => state.populateCart);
   const populateProductsList = useProductsStore((state) => state.populateProductsList);
   const handleAuthState = useAuthStore((state) => state.handleAuthState);
 
   useEffect(() => {
-    const handleAppState = async () => {
-      populateCart();
-      await populateProductsList();
-      await handleAuthState();
-
-      setLoading(false);
-    };
-
-    handleAppState();
+    populateCart();
+    populateProductsList();
+    handleAuthState();
   }, [populateCart, populateProductsList, handleAuthState]);
-
-  if (loading) return null;
 
   return (
     <Layout>
