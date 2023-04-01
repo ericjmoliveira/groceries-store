052b6620715a2983453663562740f336fa08d37f
Eric Oliveira <ericjmoliveira@gmail.com>
2023-03-31 21:46:05 -0300

    Create search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,12 +1,33 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import { useProductsStore } from '@/store/products';
+import { Card } from '@/components/card';
 
 export default function Search() {
+  const { query } = useRouter();
+  const keywords = query.query! as string;
+
+  const searchProducts = useProductsStore((state) => state.searchProducts);
+  const searchResults = searchProducts(keywords.split('-'));
+
   return (
     <>
       <Head>
         <title>Search</title>
       </Head>
-      <span>Search</span>
+      <h2 className="text-2xl font-semibold mb-8">
+        Your searched for: <em>{keywords.split('-').join(' ')}</em>
+      </h2>
+      {searchResults?.length! > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-8">
+          {searchResults?.map((product) => {
+            return <Card key={product.id} info={product} />;
+          })}
+        </div>
+      ) : (
+        <p>Your search returned no results</p>
+      )}
     </>
   );
 }
