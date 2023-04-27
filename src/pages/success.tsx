e17cabb731068f1f92f1b3bbf65e6a8b97bc2932
Eric Oliveira <ericjmoliveira@gmail.com>
2023-04-27 19:28:11 -0300

    Fix success page typo

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -19,7 +19,7 @@ export default function Success() {
       </Head>
       <h2 className="text-2xl font-semibold">Success</h2>
       <div className="flex flex-col items-center justify-center mt-32 md:mt-16">
-        <span className="text-lg font-semibold mb-4">Purchase successfull</span>
+        <span className="text-lg font-semibold mb-4">Purchase successful</span>
         <span className="mb-4">Thank you for your purchase!</span>
         <Image src={'./empty-cart.svg'} width={250} height={250} alt="Empty cart" />
         <Link href={'/'} className="underline">
